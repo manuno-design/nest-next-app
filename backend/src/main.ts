11685be9030d88dbe8eaf945ddae39e2d489d6a6
manuno-design <kidnessfree@outlook.jp>
2023-02-04 17:01:33 +0000

    add origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://nest-next-9i9hckg39-manuno-design.vercel.app'],
   });
   app.use(cookieParser());
   app.use(
